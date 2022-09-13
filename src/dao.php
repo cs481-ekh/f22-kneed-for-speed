66dcb15bf8deca39150d8abb616c3bd1a2d5e2b6
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-09-12 21:37:59 -0600

    added section for online DB info and example function for possible tests

--- src/dao.php
+++ src/dao.php
@@ -2,10 +2,19 @@
 
 class Dao {
     
+    //local
     private $host = "127.0.0.1";
     private $db = "cs481";
     private $user = "root";
     private $pw = "";
+
+    //online
+    /*
+    private $host = ;
+    private $db = ;
+    private $user = ;
+    private $pw = ;
+    */
 
     public function getConnection () {
         try{
@@ -16,6 +25,18 @@ class Dao {
             exit;
         }
     }
+
+    //Example DB function:
+    /*
+    public function createUser($userName, $password){
+        $conn = $this->getConnection(); //get a connection to DB
+        $query = "insert into users(username, password) values(:username, :password)"; //write a DB query
+        $q = $conn->prepare($query); // use prepared statement for security
+        $q->bindParam(":username", $userName); //bind parameter values
+        $q->bindParam(":password", $password);
+        $q->execute(); //execute query in DB
+    }
+    */
 }
 
 ?>
