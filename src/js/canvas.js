29223a4f954115769c5bb0db7698db1d448a5811
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-09-26 14:13:28 -0600

    canvas resizes when window is resized

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,10 @@
 var canvas = document.getElementById("map");
 var parent = document.getElementById("grid-item-5");
-canvas.width = parent.offsetWidth;
-canvas.height = parent.offsetHeight;
+canvas.width = (parent.offsetWidth*.996); //multiplication to reduce canvas size to account for 1px border
+canvas.height = (parent.offsetHeight*.996);
+
+//canvas will resize itself when window is resized
+window.onresize = function(){
+    canvas.width = (parent.offsetWidth*.996);
+    canvas.height = (parent.offsetHeight*.996);
+}
