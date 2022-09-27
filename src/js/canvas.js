b346585097529d0deae4ebc923de28904bc0dca2
Mario Torres <mariotorres791@u.boisestate.edu>
2022-09-26 23:02:50 -0600

    Added code from previous two PR's to prevent merge conflicts

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -7,4 +7,40 @@ canvas.height = (parent.offsetHeight*.996);
 window.onresize = function(){
     canvas.width = (parent.offsetWidth*.996);
     canvas.height = (parent.offsetHeight*.996);
+    drawSquare();
+    drawCircle();
 }
+
+function drawSquare() {
+    var ctx = canvas.getContext("2d");
+
+    //define the color of the square
+    ctx.strokeStyle = "blue";
+    ctx.fillStyle = "blue";
+
+    // Draw a square using the fillRect()
+    ctx.fillRect(canvas.width * 0.2, canvas.height * 0.1, 50, 50);
+
+    // Actually draws the square
+    ctx.stroke();
+}
+
+function drawCircle() {
+    var ctx = canvas.getContext("2d");
+
+    //define the color of the circle
+    ctx.strokeStyle = "blue";
+    ctx.fillStyle = "blue";
+
+    // Draw a circle using arc
+    ctx.arc(canvas.width * 0.1, canvas.height * 0.14, 25, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+    // Actually draws the circle on canvas
+    canvas.stroke();
+}
+
+// Calls function to draw square on canvas
+drawSquare();
+// Calls function to draw circle on canvas
+drawCircle();
