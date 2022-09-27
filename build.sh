590ef48fe82602bec8425ce56df3308227665876
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-09-27 10:33:59 -0600

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-exit 127
+echo Building...
+
+#Install JavaScript package manager
+sudo apt install npm
+#install JavaScript linter
+npm i eslint --save-dev
+npx eslint --init
+#install HTML linter
+npm i htmlint@0.0.7
+
+#run the linters
+npx eslint src/js/*.js
+htmlint src/*.htmlint
+
+#Zip the src folder, should include linted .js and .html files
+zip -r build.zip src
+
+exit 0
