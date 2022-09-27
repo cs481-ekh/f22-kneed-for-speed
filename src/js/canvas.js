3c705c4d231c27b5fc548f71634bccb55e3897b2
mariotorres1 <85824580+mariotorres1@users.noreply.github.com>
2022-09-27 12:06:25 -0600

    65 draw a triangle element on canvas (#69)
    * Added code from previous two PR's to prevent merge conflicts
    
    * Added drawTriangle() function
    
    * Added drawTriangle to window resizing function
    
    * Cleaned up functions
    
    Co-authored-by: Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -3,17 +3,19 @@ var parent = document.getElementById("grid-item-5");
 canvas.width = (parent.offsetWidth*.996); //multiplication to reduce canvas size to account for 1px border
 canvas.height = (parent.offsetHeight*.996);
 
-//canvas will resize itself when window is resized
+// Canvas will resize itself when window is resized
 window.onresize = function(){
     canvas.width = (parent.offsetWidth*.996);
     canvas.height = (parent.offsetHeight*.996);
     drawSquare();
     drawCircle();
+    drawTriangle();
 }
 
 function drawSquare() {
     var ctx = canvas.getContext("2d");
 
+    // Define the color of the square
     //define the color of the square
     ctx.strokeStyle = "blue";
     ctx.fillStyle = "blue";
@@ -28,12 +30,34 @@ function drawSquare() {
 function drawCircle() {
     var ctx = canvas.getContext("2d");
 
+    // Define the color of the circle
     //define the color of the circle
     ctx.strokeStyle = "blue";
     ctx.fillStyle = "blue";
 
     // Draw a circle using arc
     ctx.arc(canvas.width * 0.1, canvas.height * 0.14, 25, 0, 2 * Math.PI, false);
+
+    // Actually have circle show up on canvas
+    ctx.fill();
+}
+
+function drawTriangle() {
+    var ctx = canvas.getContext("2d");
+
+    // Define the color of the triangle
+    ctx.fillStyle = "blue";
+
+    // Drawing the triangle
+    ctx.beginPath();
+    ctx.moveTo(canvas.width * 0.15, canvas.height * 0.2);
+    ctx.lineTo(canvas.width * 0.15, canvas.height * 0.3);
+    ctx.lineTo(canvas.width * 0.25, canvas.height * 0.3);
+    ctx.closePath();
+
+    // Actually getting the triangle to show on canvas
+    ctx.fill();
+
     ctx.fill();
 
     // Actually draws the circle on canvas
@@ -44,4 +68,6 @@ function drawCircle() {
 drawSquare();
 // Calls function to draw circle on canvas
 drawCircle();
+// Calls function to draw triangle on canvas
+drawTriangle();
 
