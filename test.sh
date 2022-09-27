0a4ac36258b16c48b2cb1932823d72a8f8a0e4df
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-09-27 10:41:06 -0600

    Updated test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-exit 127
+echo Testing...
+
+#Decided to use unzip program seems its more straightforward than tar
+sudo apt install unzip
+#This directory will contain the linted src directory
+mkdir lintedDirectory
+sudo unzip build.zip -d / lintedDirectory
+
+#Test the differences between the linted directory and the src directory
+diff -q src/ lintedDirectory/
+
+exit 0
