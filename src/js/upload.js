f3918f34cb9521e0c1af49c48166b21e28c52f35
shuai-yang <shuaiyang@u.boisestate.edu>
2022-10-04 14:40:26 -0600

    made a request with upload.js

---
+++ src/js/upload.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const inputFile = document.getElementById("file");
+
+const formData = new FormData();
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    for (const file of inputFile.files) {
+        formData.append("files", file);
+    }
+};
+form.addEventListener("submit", handleSubmit);
