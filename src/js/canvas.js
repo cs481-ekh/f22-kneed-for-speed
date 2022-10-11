76e70784e08dc8c913f16ed6ba4977dbdf6aeee2
mariotorres1 <85824580+mariotorres1@users.noreply.github.com>
2022-10-10 19:23:06 -0600

    Moved 0, 0 location in drawSquare() (#96)

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -8,12 +8,11 @@ window.onresize = function(){
     canvas.width = (parent.offsetWidth*.996);
     canvas.height = (parent.offsetHeight*.996);
     drawSquare();
-    drawCircle();
-    drawTriangle();
 }
 
 function drawSquare() {
     var ctx = canvas.getContext("2d");
+    ctx.translate(canvas.width / 2, canvas.height / 2);
 
     // Define the color of the square
     //define the color of the square
@@ -21,53 +20,9 @@ function drawSquare() {
     ctx.fillStyle = "blue";
 
     // Draw a square using the fillRect()
-    ctx.fillRect(canvas.width * 0.2, canvas.height * 0.1, 50, 50);
-
-    // Actually draws the square
-    ctx.stroke();
-}
-
-function drawCircle() {
-    var ctx = canvas.getContext("2d");
-
-    // Define the color of the circle
-    //define the color of the circle
-    ctx.strokeStyle = "blue";
-    ctx.fillStyle = "blue";
-
-    // Draw a circle using arc
-    ctx.arc(canvas.width * 0.1, canvas.height * 0.14, 25, 0, 2 * Math.PI, false);
-
-    // Actually have circle show up on canvas
-    ctx.fill();
-}
-
-function drawTriangle() {
-    var ctx = canvas.getContext("2d");
-
-    // Define the color of the triangle
-    ctx.fillStyle = "blue";
-
-    // Drawing the triangle
-    ctx.beginPath();
-    ctx.moveTo(canvas.width * 0.15, canvas.height * 0.2);
-    ctx.lineTo(canvas.width * 0.15, canvas.height * 0.3);
-    ctx.lineTo(canvas.width * 0.25, canvas.height * 0.3);
-    ctx.closePath();
-
-    // Actually getting the triangle to show on canvas
-    ctx.fill();
-
-    ctx.fill();
-
-    // Actually draws the circle on canvas
-    canvas.stroke();
+    ctx.fillRect(0, 0, 50, 50);
 }
 
 // Calls function to draw square on canvas
 drawSquare();
-// Calls function to draw circle on canvas
-drawCircle();
-// Calls function to draw triangle on canvas
-drawTriangle();
 
