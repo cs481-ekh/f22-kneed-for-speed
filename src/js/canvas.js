0070b4fc13a023abdbe4161b449ea8571a3c0db5
mariotorres1 <85824580+mariotorres1@users.noreply.github.com>
2022-10-10 19:29:02 -0600

    95 hardcode some data points within our knee/circle for visual of what it will look like (#99)
    * Moved 0, 0 location in drawSquare()
    
    * Changed name of drawSqaure() to drawKnee(), also changed to circle
    
    * Have two circles being draw currently
    
    * Added 4 element positions within the knee

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -17,12 +17,92 @@ function drawKnee() {
     // Define what color the outline of our circle/knee is going to be
     ctx.strokeStyle = "black"
 
+    // Begin path
+    ctx.beginPath();
     // Creates the size of the circle/knee we're going to draw
     ctx.arc(0, 0, 100, 0, 2 * Math.PI, false);
+    // Close path
+    ctx.closePath();
 
     // Draw the outline of the circle/knee
     ctx.stroke();
+    // Added just to see the elements better because they're so small, could fill a color of the heat map and add points onto it also
+    ctx.fill();
+
+
+    // Hardcoding some elements in circle
+    drawElement();
 }
+
+function drawElement() {
+    var ctx = canvas.getContext("2d");
+
+    // Drawing red element
+    ctx.strokeStyle = "red"
+    ctx.fillStyle = "red";
+
+    // Connect x, y points given in data
+    ctx.beginPath();
+    ctx.moveTo(-35.948081, 44.580554);
+    ctx.lineTo(-35.948081, 44.580554);
+    ctx.lineTo(-36.626789, 42.016439);
+    ctx.lineTo(-37.337202, 42.335068);
+    ctx.closePath();
+
+    // Coloring in the element 
+    ctx.stroke();
+    ctx.fill();
+
+    // Changing next element to be blue
+    ctx.strokeStyle = "blue"
+    ctx.fillStyle = "blue";
+
+    // Copying points from first element in data but moving the +/- around
+    ctx.beginPath();
+    ctx.moveTo(35.948081, 44.580554);
+    ctx.lineTo(35.948081, 44.580554);
+    ctx.lineTo(36.626789, 42.016439);
+    ctx.lineTo(37.337202, 42.335068);
+    ctx.closePath();
+
+    // Coloring the new element
+    ctx.stroke();
+    ctx.fill();
+
+
+    // Changing the next element to be green
+    ctx.strokeStyle = "green"
+    ctx.fillStyle = "green";
+
+    // Copying points from first element in data but moving the +/- around
+    ctx.beginPath();
+    ctx.moveTo(35.948081, -44.580554);
+    ctx.lineTo(35.948081, -44.580554);
+    ctx.lineTo(36.626789, -42.016439);
+    ctx.lineTo(37.337202, -42.335068);
+    ctx.closePath();
+
+    // Coloring the new element
+    ctx.stroke();
+    ctx.fill();
+
+
+    // Changing the next element to be yellow
+    ctx.strokeStyle = "yellow"
+    ctx.fillStyle = "yellow";
+
+    // Copying points from first element in data but moving the +/- around
+    ctx.beginPath();
+    ctx.moveTo(-35.948081, -44.580554);
+    ctx.lineTo(-35.948081, -44.580554);
+    ctx.lineTo(-36.626789, -42.016439);
+    ctx.lineTo(-37.337202, -42.335068);
+    ctx.closePath();
+
+    // Coloring the new element
+    ctx.stroke();
+    ctx.fill();
+  }
 
 // Calls function to draw the knee
 drawKnee();
