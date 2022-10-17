7dbb8207a98c8d51760fe2f3618718862c9fd3e6
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-10-16 23:26:29 -0600

    104 enable buttons to continuously increase or decrease when held down (#115)
    * Added press and hold functionality but broke single click
    
    * fixed code to work on single clicks and while holding
    
    * trying to fix spaces and syntax for linter
    
    * still trying to fix spaces and syntax for linter
    
    * final attempt at fixing js syntax
    
    * missed deleting a semi-colon
    
    * sped up + and - on dial 2, moved location of script in index.html

--- src/js/numeric-inputs.js
+++ src/js/numeric-inputs.js
@@ -1,27 +1,86 @@
+// counter for hold interval
+let counter
+let count = 0
+
 /* Dial 1 */
 
-var num1 = document.getElementById("number-dial-1");
-var sub1 = document.getElementById("minus-dial-1");
-var add1 = document.getElementById("plus-dial-1");
+const num1 = document.getElementById('number-dial-1')
+const sub1 = document.getElementById('minus-dial-1')
+const add1 = document.getElementById('plus-dial-1')
 
-add1.onclick = function(){
-    num1.stepUp();
+// single click adds 1
+add1.onclick = function () {
+  num1.stepUp()
 }
 
-sub1.onclick = function(){
-    num1.stepDown();
+// holding mouse down continues to add 1
+add1.onmousedown = function () {
+  counter = setInterval(function () {
+    num1.innerHTML = count
+    count++
+    num1.stepUp()
+  }, 215) // speed of adding
+}
+
+// releasing mouse button resets hold counter and stops adding
+add1.onmouseup = function () {
+  clearInterval(counter)
+}
+
+// single click subtracts 1
+sub1.onclick = function () {
+  num1.stepDown()
+}
+
+// holding mouse down continues to subtract 1
+sub1.onmousedown = function () {
+  counter = setInterval(function () {
+    num1.innerHTML = count
+    count++
+    num1.stepDown()
+  }, 215) // speed of subtracting
+}
+
+// releasing mouse button resets hold counter and stops subtracting
+sub1.onmouseup = function () {
+  clearInterval(counter)
 }
 
 /* Dial 2 */
+// Works same as dial above
 
-var num2 = document.getElementById("number-dial-2");
-var sub2 = document.getElementById("minus-dial-2");
-var add2 = document.getElementById("plus-dial-2");
+const num2 = document.getElementById('number-dial-2')
+const sub2 = document.getElementById('minus-dial-2')
+const add2 = document.getElementById('plus-dial-2')
 
-add2.onclick = function(){
-    num2.stepUp();
+add2.onclick = function () {
+  num2.stepUp()
 }
 
-sub2.onclick = function(){
-    num2.stepDown();
+add2.onmousedown = function () {
+  counter = setInterval(function () {
+    num2.innerHTML = count
+    count++
+    num2.stepUp()
+  }, 215)
+}
+
+add2.onmouseup = function () {
+  clearInterval(counter)
+}
+
+sub2.onclick = function () {
+  num2.stepDown()
+}
+
+sub2.onmousedown = function () {
+  counter = setInterval(function () {
+    num2.innerHTML = count
+    count++
+    num2.stepDown()
+  }, 215)
+}
+
+sub2.onmouseup = function () {
+  clearInterval(counter)
 }
