7a2a933096698c628cdb13f536bdd3ab37c2c6ee
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-10-24 20:28:33 -0600

    almost done with linter fixes

--- src/js/parse-geometry.js
+++ src/js/parse-geometry.js
@@ -23,17 +23,17 @@ input.addEventListener('change', () => {
     const lines = file.split(/\r\n|\n/)
     let count = 0 // using 0 indexing
     lines.forEach(element => {
-      sessionStorage.setItem('line ' + count,element)
+      sessionStorage.setItem('line ' + count, element)
       console.log(sessionStorage.getItem('line ' + count)) // statement for testing
       count++
     })
 
-    let line = 0; // current line in file, using 0 indexing again
+    let line = 0 // current line in file, using 0 indexing again
 
     // find start of NODE section
     while (count >= 0) {
-      if (sessionStorage.getItem('line ' + line).search(/node/i) != (-1)) {
-        console.log('matched node on line '+ line); // statement for testing
+      if (sessionStorage.getItem('line ' + line).search(/node/i) !== (-1)) {
+        console.log('matched node on line ' + line) // statement for testing
         line++
         count--
         break
@@ -45,41 +45,39 @@ input.addEventListener('change', () => {
     let index = 0 // current node array index, using 0 indexing again
 
     // Store Node elements until Element section begins
-      while (count >= 0) {
-        if (sessionStorage.getItem('line ' + line).search(/element/i) != (-1)) {
-          console.log('matched element on line '+ line) // statement for testing
-          line++
-          count--
-          break
-        }
-
-        let temp = sessionStorage.getItem('line ' + line).split(',') // split current line on commas
-        console.log(temp) // statement for testing
-        temp.forEach(element => {
-          nodes[index].push(element) // add each comma separated value to array
-          console.log(nodes) // statement for testing
-        });
-
-        index++
-            nodes.push([]); // add new array for next node (creates multidemensional array)
-
+    while (count >= 0) {
+      if (sessionStorage.getItem('line ' + line).search(/element/i) !== (-1)) {
+        console.log('matched element on line '+ line) // statement for testing
         line++
         count--
+        break
       }
 
-      index = 0; // reset array index for elements
+      const temp = sessionStorage.getItem('line ' + line).split(',') // split current line on commas
+      console.log(temp) // statement for testing
+      temp.forEach(element => {
+        nodes[index].push(element) // add each comma separated value to array
+        console.log(nodes) // statement for testing
+      })
 
-      //Store Element elements until end of file
-      while (count >= 0) {
-        if (sessionStorage.getItem('line ' + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) != (-1)) {
-          console.log('matched end of file on line '+ line) // statement for testing
-          break
-        }
-        line++
-        count--
-      }
+      index++
+        nodes.push([]) // add new array for next node (creates multidemensional array)
+
+      line++
+      count--
     }
 
-  reader.onerror = (e) => alert(e.target.error.name)
+    index = 0 // reset array index for elements
 
+    //Store Element elements until end of file
+    while (count >= 0) {
+      if (sessionStorage.getItem('line ' + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) !== (-1)) {
+        console.log('matched end of file on line ' + line) // statement for testing
+        break
+      }
+      line++
+      count--
+    }
+  }
+  reader.onerror = (e) => alert(e.target.error.name)
 })
