dd2804641f170f25fbea683d1e9b8cff22dde6bf
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-10-24 23:49:22 -0600

    133 store parsed data as session data (#147)
    * node data successfully stored in session data
    
    * element data successfully stored in session data
    
    * tidying up code for linter round 1
    
    * tidying up code for linter round 2
    
    * missed 1 trailing space for the linter

--- src/js/parse-geometry.js
+++ src/js/parse-geometry.js
@@ -1,6 +1,6 @@
 const input = document.getElementById('file')
 
-// const submit = document.getElementById('submit') Don't need this yet
+// const submit = document.getElementById('submit') // Don't need this yet
 
 const nodes = [[]]
 const elements = [[]]
@@ -24,7 +24,7 @@ input.addEventListener('change', () => {
     let count = 0 // using 0 indexing
     lines.forEach(element => {
       sessionStorage.setItem('line ' + count, element)
-      console.log(sessionStorage.getItem('line ' + count)) // statement for testing
+      // console.log(sessionStorage.getItem('line ' + count)) // for testing
       count++
     })
 
@@ -33,7 +33,7 @@ input.addEventListener('change', () => {
     // find start of NODE section
     while (count >= 0) {
       if (sessionStorage.getItem('line ' + line).search(/node/i) !== (-1)) {
-        console.log('matched node on line ' + line) // statement for testing
+        console.log('matched node on line ' + line) // for testing
         line++
         count--
         break
@@ -47,17 +47,17 @@ input.addEventListener('change', () => {
     // Store Node elements until Element section begins
     while (count >= 0) {
       if (sessionStorage.getItem('line ' + line).search(/element/i) !== (-1)) {
-        console.log('matched element on line ' + line) // statement for testing
+        console.log('matched element on line ' + line) // for testing
         line++
         count--
         break
       }
 
       const temp = sessionStorage.getItem('line ' + line).split(',') // split current line on commas
-      console.log(temp) // statement for testing
+      // console.log(temp) // for testing
       temp.forEach(element => {
         nodes[index].push(element) // add each comma separated value to array
-        console.log(nodes) // statement for testing
+        // console.log(nodes) // for testing
       })
 
       index++
@@ -67,27 +67,77 @@ input.addEventListener('change', () => {
       count--
     }
 
+    // console.log(nodes) // for testing
+
     index = 0 // reset array index for elements
 
     // Store Element elements until end of file
     while (count >= 0) {
       if (sessionStorage.getItem('line ' + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) !== (-1)) {
-        console.log('matched end of file on line ' + line) // statement for testing
+        console.log('matched end of file on line ' + line) // for testing
         break
       }
 
       const temp = sessionStorage.getItem('line ' + line).split(',') // split current line on commas
-      console.log(temp) // statement for testing
+      // console.log(temp) // for testing
       temp.forEach(element => {
         elements[index].push(element) // add each comma separated value to array
-        console.log(elements) // statement for testing
+        // console.log(elements) // for testing
       })
 
       index++
-      elements.push([]) // add new array for next node (creates multidemensional array)
+      elements.push([]) // add new array for next element (creates multidemensional array)
 
       line++
       count--
+    }
+
+    console.log('FINAL NODE ARRAY' + '\n') // for testing
+    console.log(nodes)
+    console.log('FINAL ELEMENT ARRAY' + '\n') // for testing
+    console.log(elements)
+
+    index = 0 // reset array index for node session storage
+    let length = nodes.length // number of nodes in final node array
+    console.log('NODES SESSION DATA' + '\n' + 'total nodes: ' + length + '\n' + 'node number, x, y, z' + '\n') // for testing
+
+    while (length > 1) {
+      sessionStorage.setItem('node ' + nodes[index][0] + ' x', nodes[index][1])
+      // console.log(sessionStorage.getItem('node ' + nodes[index][0] + ' x')) // for testing
+
+      sessionStorage.setItem('node ' + nodes[index][0] + ' y', nodes[index][2])
+      // console.log(sessionStorage.getItem('node ' + nodes[index][0] + ' y')) // for testing
+
+      sessionStorage.setItem('node ' + nodes[index][0] + ' z', nodes[index][3])
+      // console.log(sessionStorage.getItem('node ' + nodes[index][0] + ' z')) // for testing
+
+      console.log(nodes[index][0] + sessionStorage.getItem('node ' + nodes[index][0] + ' x') + sessionStorage.getItem('node ' + nodes[index][0] + ' y') + sessionStorage.getItem('node ' + nodes[index][0] + ' z')) // for testing */
+
+      index++
+      length--
+    }
+
+    index = 0 // reset array index for element session storage
+    length = elements.length // reset length to number of element in final element array
+    console.log('ELEMENTS SESSION DATA' + '\n' + 'total elements: ' + length + '\n' + 'node 1, node 2, node 3, node 4' + '\n') // for testing
+
+    while (length > 1) {
+      sessionStorage.setItem('element ' + index + 'node 1', elements[index][0])
+      // console.log(sessionStorage.getItem('element ' + index + 'node 1')) // for testing
+
+      sessionStorage.setItem('element ' + index + 'node 2', elements[index][1])
+      // console.log(sessionStorage.getItem('element ' + index + 'node 2')) // for testing
+
+      sessionStorage.setItem('element ' + index + 'node 3', elements[index][2])
+      // console.log(sessionStorage.getItem('element ' + index + 'node 3')) // for testing
+
+      sessionStorage.setItem('element ' + index + 'node 4', elements[index][3])
+      // console.log(sessionStorage.getItem('element ' + index + 'node 4')) // for testing
+
+      console.log(sessionStorage.getItem('element ' + index + 'node 1') + sessionStorage.getItem('element ' + index + 'node 2') + sessionStorage.getItem('element ' + index + 'node 3') + sessionStorage.getItem('element ' + index + 'node 4')) // for testing
+
+      index++
+      length--
     }
   }
   reader.onerror = (e) => alert(e.target.error.name)
