72467b7694bd58511abf9ad5b4e6267516f99b4e
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-10-24 18:38:43 -0600

    code searches through session items and finds start of node and element sections

--- src/js/parse-geometry.js
+++ src/js/parse-geometry.js
@@ -1,7 +1,6 @@
 let input = document.getElementById('file');
 let submit = document.getElementById('submit');
 
-/* New */
 input.addEventListener('change', () => {
     let files = input.files;
 
@@ -18,10 +17,46 @@ input.addEventListener('change', () => {
         const lines = file.split(/\r\n|\n/);
         let count = 0;
         lines.forEach(element => {
-            sessionStorage.setItem(count,element);
-            console.log(sessionStorage.getItem(count));
+            sessionStorage.setItem("line " + count,element);
+            console.log(sessionStorage.getItem("line " + count));
             count++;
         });
+
+        let line = 0;
+
+        // find start of NODE section
+        while (count >= 0) {
+            if (sessionStorage.getItem("line " + line).search(/node/i) != (-1)) {
+                line++;
+                count--;
+                console.log("matched node");
+                break;
+            }
+            line++;
+            count--;
+        }
+
+        // Store Node elements until Element section begins
+        while (count >= 0) {
+            if (sessionStorage.getItem("line " + line).search(/element/i) != (-1)) {
+                line++;
+                count--;
+                console.log("matched element");
+                break;
+            }
+            line++;
+            count--;
+        }
+
+        //Store Element elements until end of file
+        while (count >= 0) {
+            if (sessionStorage.getItem("line " + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) != (-1)) {
+                console.log("matched end of file");
+                break;
+            }
+            line++;
+            count--;
+        }
     }
 
     reader.onerror = (e) => alert(e.target.error.name);
