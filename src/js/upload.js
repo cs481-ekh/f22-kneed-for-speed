e64e82ba15721806342c492c6ce9bf2dd2709fb7
shuai-yang <shuaiyang@u.boisestate.edu>
2022-10-24 20:38:21 -0600

    Error message is displayed to user if file is corrupted or not .csv

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,3 +1,37 @@
+let input = document.querySelector('input');
+
+input.addEventListener('change', () => {
+    let files = input.files;
+
+    if(files.length == 0) {
+      alert("No files chosen");
+      return;
+    }
+
+    const file = files[0];
+
+    // validation
+    let filename = file.name.toLowerCase();
+    var regex = new RegExp("(.*?)\.(csv)$"); 
+    if(!(regex.test(filename))){
+      alert("Invalid file. Please upload a csv file.");
+      return false;
+    }
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        textarea.value = lines.join('\n');
+    };
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file); 
+    
+});
+
 const form = document.getElementById('form')
 const inputFile = document.getElementById('file')
 
