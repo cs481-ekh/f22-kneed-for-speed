8f9b89574256ab656da81ee2eadbbef20187981b
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-10-24 18:44:47 -0600

    added comments and better console statements for verifying/testing

--- src/js/parse-geometry.js
+++ src/js/parse-geometry.js
@@ -12,24 +12,25 @@ input.addEventListener('change', () => {
 
     reader.readAsText(file);
 
+    //Reads in file line by line and stores each line as sessionStorage item
     reader.onload = (e) => {
         const file = e.target.result;
         const lines = file.split(/\r\n|\n/);
-        let count = 0;
+        let count = 0; // using 0 indexing
         lines.forEach(element => {
             sessionStorage.setItem("line " + count,element);
             console.log(sessionStorage.getItem("line " + count));
             count++;
         });
 
-        let line = 0;
+        let line = 0; // current line, using 0 indexing again
 
         // find start of NODE section
         while (count >= 0) {
             if (sessionStorage.getItem("line " + line).search(/node/i) != (-1)) {
+                console.log("matched node on line "+ line);
                 line++;
                 count--;
-                console.log("matched node");
                 break;
             }
             line++;
@@ -39,9 +40,9 @@ input.addEventListener('change', () => {
         // Store Node elements until Element section begins
         while (count >= 0) {
             if (sessionStorage.getItem("line " + line).search(/element/i) != (-1)) {
+                console.log("matched element on line "+ line);
                 line++;
                 count--;
-                console.log("matched element");
                 break;
             }
             line++;
@@ -51,7 +52,7 @@ input.addEventListener('change', () => {
         //Store Element elements until end of file
         while (count >= 0) {
             if (sessionStorage.getItem("line " + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) != (-1)) {
-                console.log("matched end of file");
+                console.log("matched end of file on line "+ line);
                 break;
             }
             line++;
