38b42f1db0118e1cde02d45b2c1844c2cbc9fec5
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-10-24 20:20:37 -0600

    changed double quotes to single for linter

--- src/js/parse-geometry.js
+++ src/js/parse-geometry.js
@@ -7,33 +7,33 @@ const nodes = [[]]
 
 // runs as soon as a user selects a file - needs some additional file-type safety
 input.addEventListener('change', () => {
-  let files = input.files
+  const files = input.files
 
-  if(files.length == 0) return
+  if (files.length === 0) return
 
   const file = files[0]
 
-  let reader = new FileReader()
+  const reader = new FileReader()
 
   reader.readAsText(file)
 
-  //Reads in file line by line and stores each line as sessionStorage item
+  // Reads in file line by line and stores each line as sessionStorage item
   reader.onload = (e) => {
     const file = e.target.result
     const lines = file.split(/\r\n|\n/)
     let count = 0 // using 0 indexing
     lines.forEach(element => {
-      sessionStorage.setItem("line " + count,element)
-      console.log(sessionStorage.getItem("line " + count)) // statement for testing
+      sessionStorage.setItem('line ' + count,element)
+      console.log(sessionStorage.getItem('line ' + count)) // statement for testing
       count++
-    });
+    })
 
     let line = 0; // current line in file, using 0 indexing again
 
     // find start of NODE section
     while (count >= 0) {
-      if (sessionStorage.getItem("line " + line).search(/node/i) != (-1)) {
-        console.log("matched node on line "+ line); // statement for testing
+      if (sessionStorage.getItem('line ' + line).search(/node/i) != (-1)) {
+        console.log('matched node on line '+ line); // statement for testing
         line++
         count--
         break
@@ -46,14 +46,14 @@ input.addEventListener('change', () => {
 
     // Store Node elements until Element section begins
       while (count >= 0) {
-        if (sessionStorage.getItem("line " + line).search(/element/i) != (-1)) {
-          console.log("matched element on line "+ line) // statement for testing
+        if (sessionStorage.getItem('line ' + line).search(/element/i) != (-1)) {
+          console.log('matched element on line '+ line) // statement for testing
           line++
           count--
           break
         }
 
-        let temp = sessionStorage.getItem("line " + line).split(',') // split current line on commas
+        let temp = sessionStorage.getItem('line ' + line).split(',') // split current line on commas
         console.log(temp) // statement for testing
         temp.forEach(element => {
           nodes[index].push(element) // add each comma separated value to array
@@ -71,8 +71,8 @@ input.addEventListener('change', () => {
 
       //Store Element elements until end of file
       while (count >= 0) {
-        if (sessionStorage.getItem("line " + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) != (-1)) {
-          console.log("matched end of file on line "+ line) // statement for testing
+        if (sessionStorage.getItem('line ' + line).search(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm) != (-1)) {
+          console.log('matched end of file on line '+ line) // statement for testing
           break
         }
         line++
