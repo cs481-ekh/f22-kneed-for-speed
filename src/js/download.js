5167e304038ed4abb9b81dca40f478fee314c06f
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-10-27 16:05:15 -0600

    Changed File Format to .png

--- src/js/download.js
+++ src/js/download.js
@@ -1,8 +1,8 @@
 function downloadImage() {
     var canvas = document.getElementById("map");
-    image = canvas.toDataURL("image/jpeg");
+    image = canvas.toDataURL("image/png");
     var link = document.createElement('a');
-    link.download = "my-image.jpeg";
+    link.download = "my-image.png";
     link.href = image;
     link.click();
 }
