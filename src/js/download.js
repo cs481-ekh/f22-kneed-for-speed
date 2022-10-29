00842c49e28642744e3a51a3f475308e0aaab811
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-10-29 14:44:35 -0600

    Download.js is now an Internal Script

--- src/js/download.js
+++
@@ -1,8 +0,0 @@
-function downloadImage() {
-    var canvas = document.getElementById("map");
-    image = canvas.toDataURL("image/png");
-    var link = document.createElement('a');
-    link.download = "my-image.png";
-    link.href = image;
-    link.click();
-}
