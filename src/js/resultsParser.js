87e6b149032c4f1f9cadd7592c0b949d6390eedc
mitchcrocker <mitchellcrocker@u.boisestate.edu>
2022-11-03 16:21:13 -0600

    linter fixes

--- src/js/resultsParser.js
+++ src/js/resultsParser.js
@@ -4,7 +4,7 @@ const resultOutput = [[]]
 
 resultInput.addEventListener('change', () => {
   const resultFiles = resultInput.files
-  
+
   if (resultFiles.length === 0) return
 
   const file = resultFiles[0]
@@ -16,7 +16,7 @@ resultInput.addEventListener('change', () => {
     const resLines = resFile.split(/\r\/|\n/)
     console.log(resLines.length)
     let indexCount = resLines.length
-   
+
     let index = 0
     while (indexCount > 0) {
       const placehold = resLines[index].split(',')
@@ -27,9 +27,9 @@ resultInput.addEventListener('change', () => {
       index++
       indexCount--
     }
-    console.log("First elements results: " + resultOutput[0])
-    console.log("Element Num " + resultOutput[0][0])
-    console.log("first time " + resultOutput[0][1])
-    console.log("last time " + resultOutput[0][240])
+    console.log('First elements results: ' + resultOutput[0])
+    console.log('Element Num ' + resultOutput[0][0])
+    console.log('first time ' + resultOutput[0][1])
+    console.log('last time ' + resultOutput[0][240])
   }
 })
