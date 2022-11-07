036ba122ee843fdac480fa9efb80cf14fd2687bd
Mario Torres <mariotorres791@u.boisestate.edu>
2022-11-06 20:29:12 -0700

    accessing draw button

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('map')
 const parent = document.getElementById('canvas-container')
+const draw = document.getElementById('draw')
 const colorList = { red: '#FF0000', orange_red: '#FF4500', yellow: '#FFFF00', green_yellow: '#ADFF20', green: '#008000', teal: '#008080', light_blue: '#ADD8E0', blue: '#0000F0' }
 canvas.width = (parent.offsetWidth * 0.996) // multiplication to reduce canvas size to account for 1px border
 canvas.height = (parent.offsetHeight * 0.996)
