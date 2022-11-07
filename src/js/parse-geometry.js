86c346311fb4ea64da165e237d81974e286a85c4
katerosenthal <45341243+katerosenthal@users.noreply.github.com>
2022-11-07 15:25:09 -0700

    197 update element session data storage and naming conventions (#200)
    * updated element session data naming conventions and print statements
    
    * store total number of elements and initial element number is session data
    
    * removed an extra space for the linter

--- src/js/parse-geometry.js
+++ src/js/parse-geometry.js
@@ -131,22 +131,24 @@ input.addEventListener('change', () => {
 
     index = 0 // reset array index for element session storage
     length = elements.length // reset length to number of element in final element array
-    console.log('ELEMENTS SESSION DATA' + '\n' + 'total elements: ' + length + '\n' + 'node 1, node 2, node 3, node 4' + '\n') // for testing
+
+    sessionStorage.setItem('total elements', length) // store total number of elements in session data
+    sessionStorage.setItem('initial element number', elements[index][0]) // store initial element number in session data
+
+    console.log('ELEMENTS SESSION DATA' + '\n' + 'total elements: ' + sessionStorage.getItem('total elements') + '\n' + 'element number, node 1, node 2, node 3' + '\n') // for testing
+    console.log('starting element number: ' + sessionStorage.getItem('initial element number') + '\n') // for testing
 
     while (length > 1) {
-      sessionStorage.setItem('element ' + index + 'node 1', elements[index][0])
-      // console.log(sessionStorage.getItem('element ' + index + 'node 1')) // for testing
+      sessionStorage.setItem('element ' + elements[index][0] + ' node 1', elements[index][1])
+      // console.log(sessionStorage.getItem('element ' + elements[index][0] + ' node 1')) // for testing
 
-      sessionStorage.setItem('element ' + index + 'node 2', elements[index][1])
-      // console.log(sessionStorage.getItem('element ' + index + 'node 2')) // for testing
+      sessionStorage.setItem('element ' + elements[index][0] + ' node 2', elements[index][2])
+      // console.log(sessionStorage.getItem('element ' + elements[index][0] + ' node 2')) // for testing
 
-      sessionStorage.setItem('element ' + index + 'node 3', elements[index][2])
-      // console.log(sessionStorage.getItem('element ' + index + 'node 3')) // for testing
+      sessionStorage.setItem('element ' + elements[index][0] + ' node 3', elements[index][3])
+      // console.log(sessionStorage.getItem('element ' + elements[index][0] + ' node 3')) // for testing
 
-      sessionStorage.setItem('element ' + index + 'node 4', elements[index][3])
-      // console.log(sessionStorage.getItem('element ' + index + 'node 4')) // for testing
-
-      console.log(sessionStorage.getItem('element ' + index + 'node 1') + ' ' + sessionStorage.getItem('element ' + index + 'node 2') + ' ' + sessionStorage.getItem('element ' + index + 'node 3') + ' ' + sessionStorage.getItem('element ' + index + 'node 4')) // for testing
+      console.log(elements[index][0] + ' ' + sessionStorage.getItem('element ' + elements[index][0] + ' node 1') + ' ' + sessionStorage.getItem('element ' + elements[index][0] + ' node 2') + ' ' + sessionStorage.getItem('element ' + elements[index][0] + ' node 3')) // for testing
 
       index++
       length--
