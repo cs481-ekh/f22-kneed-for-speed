49daaf2789545390abf00197e2fd9ce0210a3ec5
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-11-10 14:38:30 -0700

    Added some broken functions

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,34 +1,23 @@
 const canvas = document.getElementById('map')
 const parent = document.getElementById('canvas-container')
 const draw = document.getElementById('draw')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
 const createdNodes = [[]]
 const colorList = { red: '#FF0000', orange_red: '#FF4500', yellow: '#FFFF00', green_yellow: '#ADFF20', green: '#008000', teal: '#008080', light_blue: '#ADD8E0', blue: '#0000F0' }
 canvas.width = (parent.offsetWidth * 0.996) // multiplication to reduce canvas size to account for 1px border
 canvas.height = (parent.offsetHeight * 0.996)
 
-// Canvas will resize itself when window is resized
-window.onresize = function () {
-  canvas.width = (parent.offsetWidth * 0.996)
-  canvas.height = (parent.offsetHeight * 0.996)
-  drawKnee()
-}
-
-// Draws on canvas when draw button is pushed after selecting files
-draw.onclick = function () {
-  draw.remove()
-  drawKnee()
-}
-
-function drawKnee () {
+function drawKnee(scale, translatePos) {
   createNodes()
   const ctx = canvas.getContext('2d')
-  ctx.translate(canvas.width / 1.7, canvas.height / 20)
+  ctx.translate(translatePos.x, translatePos.y)
 
-  ctx.scale(6, 6)
+  ctx.scale(scale, scale)
   drawElement()
 }
 
-function drawElement () {
+function drawElement() {
   const ctx = canvas.getContext('2d')
 
   // Loop through createdNodes and draw each node
@@ -69,14 +58,76 @@ function drawElement () {
   }
 }
 
-function createNodes () {
+var translatePos = {
+  x: canvas.width / 1.75,
+  y: canvas.height / 30
+};
+
+var scale = 6.0;
+var scaleMultiplier = 5.0;
+var startDragOffset = {};
+var mouseDown = false;
+
+plus.addEventListener("click", function () {
+  console.log("Zoom in");
+  scale /= scaleMultiplier;
+  drawKnee(scale, translatePos);
+}, false);
+
+minus.addEventListener("click", function () {
+  console.log("Zoom out");
+  scale *= scaleMultiplier;
+  drawKnee(scale, translatePos);
+}, false);
+
+canvas.addEventListener("mousedown", function(evt) {
+  console.log("mouse down")
+  mouseDown = true;
+  startDragOffset.x = evt.clientX - translatePos.x;
+  startDragOffset.y = evt.clientY - translatePos.y;
+});
+
+canvas.addEventListener("mousemove", function(evt) {
+  if (mouseDown) {
+    translatePos.x = evt.clientX - startDragOffset.x;
+    translatePos.y = evt.clientY - startDragOffset.y;
+    drawKnee(scale, translatePos);
+  }
+});
+
+canvas.addEventListener("mouseup", function(evt) {
+  mouseDown = false;
+});
+
+canvas.addEventListener("mouseover", function(evt) {
+  mouseDown = false;
+});
+
+canvas.addEventListener("mouseout", function(evt) {
+  mouseDown = false;
+});
+
+// Canvas will resize itself when window is resized
+window.onresize = function () {
+  canvas.width = (parent.offsetWidth * 0.996)
+  canvas.height = (parent.offsetHeight * 0.996)
+  drawKnee(scale, translatePos)
+}
+
+// Draws on canvas when draw button is pushed after selecting files
+draw.onclick = function () {
+  draw.remove()
+  drawKnee(scale, translatePos)
+}
+
+function createNodes() {
   for (let i = 0; i < nodes.length; i++) { // eslint-disable-line
     const n = new Node(nodes[i][0], nodes[i][1], nodes[i][2], Math.random()) // eslint-disable-line
     createdNodes.push(n)
   }
 }
 
-function heatmapKey (colorList) {
+function heatmapKey(colorList) {
   const heatmapKey = document.getElementById('heatmap')
 
   for (const key in colorList) {
@@ -96,7 +147,7 @@ function heatmapKey (colorList) {
 }
 
 class Node {
-  constructor (id, xVal, yVal, force) {
+  constructor(id, xVal, yVal, force) {
     this.id = id
     this.xVal = xVal
     this.yVal = yVal
