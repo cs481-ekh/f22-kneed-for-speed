22cbe4e5e84d82af9cc971fb19966e9f98c05037
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-11-28 21:56:52 -0700

    Revert back to Mitchell's commit

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,185 +1,159 @@
-// Creating variables
-const canvas = document.getElementById('map');
-const parent = document.getElementById('canvas-container');
-const draw = document.getElementById('draw');
-let createdNodes = [[]];
-const colorList = { red: '#FF0000', orange_red: '#FF4500', yellow: '#FFFF00', green_yellow: '#ADFF20', green: '#008000', teal: '#008080', light_blue: '#ADD8E0', blue: '#0000F0' };
+const canvas = document.getElementById('map')
+const parent = document.getElementById('canvas-container')
+const draw = document.getElementById('draw')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const createdNodes = [[]]
+const colorList = { red: '#FF0000', orange_red: '#FF4500', yellow: '#FFFF00', green_yellow: '#ADFF20', green: '#008000', teal: '#008080', light_blue: '#ADD8E0', blue: '#0000F0' }
+canvas.width = (parent.offsetWidth * 0.996) // multiplication to reduce canvas size to account for 1px border
+canvas.height = (parent.offsetHeight * 0.996)
 
-// Adjusting width and height of canvas
-canvas.width = (parent.offsetWidth * 0.996); // multiplication to reduce canvas size to account for 1px border
-canvas.height = (parent.offsetHeight * 0.996);
+function drawKnee(scale, translatePos) {
+  createNodes()
+  const ctx = canvas.getContext('2d')
+  ctx.translate(translatePos.x, translatePos.y)
 
-// Moving 0, 0 to middle of canvas and making drawing bigger and easier to view
-const ctx = canvas.getContext('2d');
-ctx.translate(canvas.width / 2, 0);
-ctx.scale(6, 6);
+  ctx.scale(scale, scale)
+  drawElement()
+}
 
-// creating the force spreads
-const n = 8;
-let rangeDiff = ((highestForce - lowestForce) / (n - 1)); // eslint-disable-line
-let forceRanges = [(lowestForce + rangeDiff * 0), (lowestForce + rangeDiff * 1), (lowestForce + rangeDiff * 2), (lowestForce + rangeDiff * 3), (lowestForce + rangeDiff * 4), (lowestForce + rangeDiff * 5), (lowestForce + rangeDiff * 6), (lowestForce + rangeDiff * 7)]; // eslint-disable-line
+function drawElement() {
+  const ctx = canvas.getContext('2d')
+
+  // Loop through createdNodes and draw each node
+  for (let i = 0; i < createdNodes.length; i++) {
+    if (createdNodes[i].force <= 0.5) {
+      ctx.strokeStyle = '#0000F0'
+      ctx.fillStyle = '#0000F0'
+    } else if (createdNodes[i].force <= 0.6) {
+      ctx.strokeStyle = '#ADD8E0'
+      ctx.fillStyle = '#ADD8E0'
+    } else if (createdNodes[i].force <= 0.7) {
+      ctx.strokeStyle = '#008080'
+      ctx.fillStyle = '#008080'
+    } else if (createdNodes[i].force <= 0.75) {
+      ctx.strokeStyle = '#008000'
+      ctx.fillStyle = '#008000'
+    } else if (createdNodes[i].force <= 0.8) {
+      ctx.strokeStyle = '#ADFF20'
+      ctx.fillStyle = '#ADFF20'
+    } else if (createdNodes[i].force <= 0.85) {
+      ctx.strokeStyle = '#FFFF00'
+      ctx.fillStyle = '#FFFF00'
+    } else if (createdNodes[i].force <= 0.9) {
+      ctx.strokeStyle = '#FF4500'
+      ctx.fillStyle = '#FF4500'
+    } else {
+      ctx.strokeStyle = '#FF0000'
+      ctx.fillStyle = '#FF0000'
+    }
+
+    ctx.beginPath()
+    // Made radius bigger to make it easier to see to discuss how we want to move forward
+    ctx.fillRect(createdNodes[i].xVal, createdNodes[i].yVal, 3, 3)
+    ctx.closePath()
+
+    ctx.stroke()
+    ctx.fill()
+  }
+}
+
+var translatePos = {
+  x: canvas.width / 1.75,
+  y: canvas.height / 30
+};
+
+var scale = 6.0;
+var scaleMultiplier = 5.0;
+var startDragOffset = {};
+var mouseDown = false;
+
+plus.addEventListener("click", function () {
+  console.log("Zoom in");
+  scale /= scaleMultiplier;
+  drawKnee(scale, translatePos);
+}, false);
+
+minus.addEventListener("click", function () {
+  console.log("Zoom out");
+  scale *= scaleMultiplier;
+  drawKnee(scale, translatePos);
+}, false);
+
+canvas.addEventListener("mousedown", function(evt) {
+  console.log("mouse down")
+  mouseDown = true;
+  startDragOffset.x = evt.clientX - translatePos.x;
+  startDragOffset.y = evt.clientY - translatePos.y;
+});
+
+canvas.addEventListener("mousemove", function(evt) {
+  if (mouseDown) {
+    translatePos.x = evt.clientX - startDragOffset.x;
+    translatePos.y = evt.clientY - startDragOffset.y;
+    drawKnee(scale, translatePos);
+  }
+});
+
+canvas.addEventListener("mouseup", function(evt) {
+  mouseDown = false;
+});
+
+canvas.addEventListener("mouseover", function(evt) {
+  mouseDown = false;
+});
+
+canvas.addEventListener("mouseout", function(evt) {
+  mouseDown = false;
+});
 
 // Canvas will resize itself when window is resized
 window.onresize = function () {
-  // Adjusting canvas width and height
-  canvas.width = (parent.offsetWidth * 0.996);
-  canvas.height = (parent.offsetHeight * 0.996);
-  drawKnee();
+  canvas.width = (parent.offsetWidth * 0.996)
+  canvas.height = (parent.offsetHeight * 0.996)
+  drawKnee(scale, translatePos)
 }
 
 // Draws on canvas when draw button is pushed after selecting files
 draw.onclick = function () {
-  draw.disabled = true;
-  drawKnee();
+  draw.remove()
+  drawKnee(scale, translatePos)
 }
 
-// Draws the knee with data from files selected to canvas
-function drawKnee (scale, translatePos) {
-  createNodes();
-  drawElement(scale, translatePos);
-  recalculateHeatmapForces();
-}
-
-// Changes the heatmap values based on data passed in
-function recalculateHeatmapForces () {
-  rangeDiff = ((highestForce - lowestForce) / (n - 1)); // eslint-disable-line
-  forceRanges = [(lowestForce + rangeDiff * 0), (lowestForce + rangeDiff * 1), (lowestForce + rangeDiff * 2), (lowestForce + rangeDiff * 3), (lowestForce + rangeDiff * 4), (lowestForce + rangeDiff * 5), (lowestForce + rangeDiff * 6), (lowestForce + rangeDiff * 7)]; // eslint-disable-line
-  heatmapKey(colorList);
-}
-
-// Draws each element from data on the canvas
-function drawElement (scale, translatePos) {
-  const ctx = canvas.getContext("2d");
-  clearCanvas();
-
-  // Loop through createdNodes and set the fill and stroke color based on the force associated with each node
-  for (let i = 0; i < createdNodes.length; i++) {
-    if (createdNodes[i].force <= 0.5) {
-      ctx.strokeStyle = '#0000F0';
-      ctx.fillStyle = '#0000F0';
-    } else if (createdNodes[i].force <= 0.6) {
-      ctx.strokeStyle = '#ADD8E0';
-      ctx.fillStyle = '#ADD8E0';
-    } else if (createdNodes[i].force <= 0.7) {
-      ctx.strokeStyle = '#008080';
-      ctx.fillStyle = '#008080';
-    } else if (createdNodes[i].force <= 0.75) {
-      ctx.strokeStyle = '#008000';
-      ctx.fillStyle = '#008000';
-    } else if (createdNodes[i].force <= 0.8) {
-      ctx.strokeStyle = '#ADFF20';
-      ctx.fillStyle = '#ADFF20';
-    } else if (createdNodes[i].force <= 0.85) {
-      ctx.strokeStyle = '#FFFF00';
-      ctx.fillStyle = '#FFFF00';
-    } else if (createdNodes[i].force <= 0.9) {
-      ctx.strokeStyle = '#FF4500';
-      ctx.fillStyle = '#FF4500';
-    } else {
-      ctx.strokeStyle = '#FF0000';
-      ctx.fillStyle = '#FF0000';
-    }
-
-    // Can begin drawing
-    ctx.save();
-    ctx.translate(translatePos.x, translatePos.y);
-    ctx.scale(scale, scale);
-    ctx.beginPath();
-    // Using rectangles to draw our knee on the canvas
-    ctx.fillRect(createdNodes[i].xVal, createdNodes[i].yVal, 3, 3);
-    // Stop drawing
-    ctx.closePath();
-
-    // Colors the rectangle on canvas
-    ctx.stroke();
-    ctx.fill();
-  }
-}
-
-// Loops through the data from file passed in creating nodes to be drawn on canvas
-function createNodes () {
+function createNodes() {
   for (let i = 0; i < nodes.length; i++) { // eslint-disable-line
-    const n = new Node(nodes[i][0], nodes[i][1], nodes[i][2], Math.random()); // eslint-disable-line
-    createdNodes.push(n);
+    const n = new Node(nodes[i][0], nodes[i][1], nodes[i][2], Math.random()) // eslint-disable-line
+    createdNodes.push(n)
   }
 }
 
-// Clears the canvas of a drawing and clears the data of nodes we created
-function clearCanvas () {
-  const ctx = canvas.getContext('2d');
+function heatmapKey(colorList) {
+  const heatmapKey = document.getElementById('heatmap')
 
-  ctx.clearRect(-canvas.width / 2, 0, canvas.width, canvas.height);
-  createdNodes = [[]];
-}
-
-// Creates the heatmap legend
-function heatmapKey (colorList) {
-  // Variable
-  const heatmapKey = document.getElementById('heatmap');
-
-  // Looping through and clearing any child nodes of the variable we created
-  while (heatmapKey.firstChild) {
-    heatmapKey.removeChild(heatmapKey.firstChild);
-  }
-  let i = 0;
-
-  // Creating each item in our heatmap legend
   for (const key in colorList) {
-    const boxContainer = document.createElement('div');
-    const box = document.createElement('div');
-    const label = document.createElement('span');
+    const boxContainer = document.createElement('div')
+    const box = document.createElement('div')
+    // var label = document.createElement("span")
 
-    label.innerHTML = key;
-    box.className = 'box';
-    box.style.backgroundColor = colorList[key];
+    // label.innerHTML = key
+    box.className = 'box'
+    box.style.backgroundColor = colorList[key]
 
-    // Setting the max and min values in the heatmap legend
-    if (i === 0) {
-      const forceLabel = forceRanges[7].toFixed(5);
-      boxContainer.append(forceLabel + ' ');
-    }
-    if (i === 7) {
-      const forceLabel = forceRanges[0].toFixed(5);
-      boxContainer.append(forceLabel + ' ');
-    }
+    boxContainer.appendChild(box)
+    // boxContainer.appendChild(label)
 
-    boxContainer.appendChild(box);
-
-    heatmapKey.appendChild(boxContainer);
-    i++;
+    heatmapKey.appendChild(boxContainer)
   }
 }
 
-// Node class
 class Node {
-  constructor (id, xVal, yVal, force) {
-    this.id = id;
-    this.xVal = xVal;
-    this.yVal = yVal;
-    this.force = force;
+  constructor(id, xVal, yVal, force) {
+    this.id = id
+    this.xVal = xVal
+    this.yVal = yVal
+    this.force = force
   }
 }
 
-window.onload = function() {
-  var canvas = document.getElementById("map");
-
-  var translatePos = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-  };
-
-  var scale = 1.0;
-  var scaleMultiplier = 0.8;
-  var dragOffset = {};
-  var mouseDown = false;
-
-  document.getElementById("plus").addEventListener("click", function() {
-    scale /= scaleMultiplier;
-    drawKnee(scale, translatePos);
-  }, false);
-
-  document.getElementById("minus").addEventListener("click", function() {
-    scale *= scaleMultiplier;
-    drawKnee(scale, translatePos);
-  }, false);
-}
+// Calls function to create our heatmap key
+heatmapKey(colorList)
